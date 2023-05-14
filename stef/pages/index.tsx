048fed3320cefc8e38ceb3c5a046e06e1e65ba8c
dserra22 <75829857+dserra22@users.noreply.github.com>
2023-05-14 09:55:20 -0700

    again

--- stef/pages/index.tsx
+++ stef/pages/index.tsx
@@ -10,50 +10,50 @@ const boundaries = 15; // how big
 
 // get an svg of a tree
 export default function Home() {
-  const [worldSpin, setWorldSpin] = useState(0);
+  // const [worldSpin, setWorldSpin] = useState(0);
 
-  const [backgroundMover, setBackgroundMover] = useState(boundaries);
-  const [changeDirection, setChangeDirection] = useState(-1);
-  const [isChanged, setChange] = useState(false);
+  // const [backgroundMover, setBackgroundMover] = useState(boundaries);
+  // const [changeDirection, setChangeDirection] = useState(-1);
+  // const [isChanged, setChange] = useState(false);
 
-  useEffect(() => {
-    // we create an interval function that adds changeDirection to backgroundMover
-    // backgroundMover += changeDirection is new background position
-    const timerFunction = () => {
-      setBackgroundMover((prevState: number) => {
-        prevState += changeDirection;
-        return prevState;
-      });
-      setWorldSpin((prevState: number) => {
-        prevState += 1;
-        return prevState;
-      });
-    };
-    const time = setInterval(timerFunction, 20); // speed
+  // useEffect(() => {
+  //   // we create an interval function that adds changeDirection to backgroundMover
+  //   // backgroundMover += changeDirection is new background position
+  //   const timerFunction = () => {
+  //     setBackgroundMover((prevState: number) => {
+  //       prevState += changeDirection;
+  //       return prevState;
+  //     });
+  //     setWorldSpin((prevState: number) => {
+  //       prevState += 1;
+  //       return prevState;
+  //     });
+  //   };
+  //   const time = setInterval(timerFunction, 20); // speed
 
-    return () => clearInterval(time);
-  }, [backgroundMover]);
+  //   return () => clearInterval(time);
+  // }, [backgroundMover]);
 
-  useEffect(() => {
-    // We must only change setChangeDirection once
-    if (Math.abs(backgroundMover) >= boundaries + 1 && isChanged === false) {
-      setChange(true);
-      setChangeDirection((prevDirection) => 0 - prevDirection); // flip direction
-    }
-    // we turn it back on once we've hit 0.
-    if (backgroundMover === 0) {
-      setChange(false);
-    }
-  }, [backgroundMover]);
+  // useEffect(() => {
+  //   // We must only change setChangeDirection once
+  //   if (Math.abs(backgroundMover) >= boundaries + 1 && isChanged === false) {
+  //     setChange(true);
+  //     setChangeDirection((prevDirection) => 0 - prevDirection); // flip direction
+  //   }
+  //   // we turn it back on once we&apos;ve hit 0.
+  //   if (backgroundMover === 0) {
+  //     setChange(false);
+  //   }
+  // }, [backgroundMover]);
 
-  // console.log(backgroundMover);
-  console.log(backgroundMover * 0.01 + 2);
+  // // console.log(backgroundMover);
+  // console.log(backgroundMover * 0.01 + 2);
 
   let allPages: any = [
     {
       first: (
         <>
-          <h1 className={styles.happyBirthday}>Happy Mother's day Mom!</h1>
+          <h1 className={styles.happyBirthday}>Happy Mother&apos;s day Mom!</h1>
           <p className={styles.abre}>Open the page!</p>
         </>
       ),
@@ -61,10 +61,10 @@ export default function Home() {
         <>
           <p className={styles.desc}>Dear Mom,</p>
           <p className={styles.desc}>
-            Happy Mother's day! The last time I saw you, you stalked me at the
-            airport ✈️ watching me get my boarding pass. That was really cute,
-            and it goes to say how far you go for me. It's been like this my
-            whole life, with all of the support you have given me. 😊
+            Happy Mother&apos;s day! The last time I saw you, you stalked me at
+            the airport ✈️ watching me get my boarding pass. That was really
+            cute, and it goes to say how far you go for me. It&apos;s been like
+            this my whole life, with all of the support you have given me. 😊
           </p>
         </>
       ),
@@ -87,7 +87,7 @@ export default function Home() {
   const [onPage, changePage] = useState(0);
 
   const pages = allPages.map((page: number, i: number) => {
-    // onPage = 0; page = 0; we're on page 0
+    // onPage = 0; page = 0; we&apos;re on page 0
     // onPage = 1; page = 0; flip
     let zIndex: number = 0;
 
@@ -140,7 +140,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Happy Mother's day {birthdayName}</title>
+        <title>Happy Mother&apos;s day {birthdayName}</title>
       </Head>
 
       <section className={styles.section}>
